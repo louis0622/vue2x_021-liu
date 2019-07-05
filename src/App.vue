6412becec5be074a0bc6acaa4d67eefa954c5fb5
liujie <64555555@qq.com>
2019-07-05 22:10:33 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+      <FooterGuide></FooterGuide>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide'
+export default {
+  name: 'App',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus" scoped>
+  
+  
+</style>
